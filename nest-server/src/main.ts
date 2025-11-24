115a5bc15bd95dc2e90feaa533302dd807cec89d
clr1235 <15035291867@163.com>
2025-11-24 14:58:40 +0800

    chore: 修改配置字段

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -7,9 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 获取配置服务
   const configService = app.get(ConfigService);
-
+  console.log(configService.get('nestConf.globalPrefix'), 'globalPrefix');
+  const globalPrefix = configService.get('nestConf.globalPrefix') as string;
   // 设置全局路由前缀
-  app.setGlobalPrefix(configService.get('nestconf.globalPrefix') as string);
+  app.setGlobalPrefix(globalPrefix);
   // web 安全，防常见漏洞
   // 注意： 开发环境如果开启 nest static module 需要将 crossOriginResourcePolicy 设置为 false 否则 静态资源 跨域不可访问
   app.use(
@@ -27,7 +28,8 @@ async function bootstrap() {
     allowedHeaders: configService.get('nestConf.enableCorsAllowedHeaders'), // 按需配置允许的请求头
   });
 
-  const port = configService.get('nest.port') as number;
+  const port = configService.get('nestConf.port') as number;
+  console.log(port, 'port');
   await app.listen(port);
 }
 void bootstrap();
