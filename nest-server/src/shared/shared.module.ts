bca2e5d8bb4cc3dc3a376a2a5ce7a80b97847799
clr1235 <15035291867@163.com>
2025-11-27 11:19:01 +0800

    feat: 添加LoggerModule

--- nest-server/src/shared/shared.module.ts
+++ nest-server/src/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SharedService } from './shared.service';
 import { RedisModule } from './redis/redis.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [RedisModule, PrismaModule],
+  imports: [RedisModule, PrismaModule, LoggerModule.forRoot()],
   providers: [SharedService],
   exports: [SharedService, RedisModule, PrismaModule],
 })
