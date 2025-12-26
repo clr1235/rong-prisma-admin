f6c7e3d796242f5fb5d6cb820e4a637eada4e83e
clr1235 <15035291867@163.com>
2025-12-26 10:35:14 +0800

    feat: 使用@Public装饰器将login、register等接口设置为绕过jwt认证

--- nest-server/src/auth/controllers/auth.controller.ts
+++ nest-server/src/auth/controllers/auth.controller.ts
@@ -10,6 +10,7 @@ import {
 import { AuthService } from '../auth.service';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
 import { RegisterDto } from '../dto';
+import { Public } from '../decorators/public.decorator';
 
 // 使用passport-local 策略提供的内置 AuthGuard 来装饰路由意味着：只有在用户通过验证后才会调用路由处理程序
 @UseGuards(LocalAuthGuard)
@@ -18,6 +19,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @Public()
   async login(@Request() req) {
     // Passport 会根据 validate() 方法的返回值自动创建 user 对象，并将其赋值给 Request 对象的 req.user 属性。
     // 所以此处可以在req中取到user对象
@@ -25,11 +27,13 @@ export class AuthController {
   }
 
   @Post('logout')
+  @Public()
   async logout(@Request() req) {
     return req.logout();
   }
 
   @Post('register')
+  @Public()
   async register(@Body() dto: RegisterDto): Promise<void> {
     await this.authService.register(dto);
   }
